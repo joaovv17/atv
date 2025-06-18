ce0fe9b5df80b4c1a7d4ba58d187fe677531d486
joaovv17 <joaovibarboa@gmail.com>
2025-06-18 17:40:44 -0300

    Add files via upload

---
+++ crim.js
@@ -0,0 +1,12 @@
+let anos=70;
+let nulo = null;
+let nome = "jao";
+
+console.log(anos);
+console.log(typeof(anos));
+
+console.log(nulo);
+console.log(typeof(nulo));
+
+console.log(nome);
+console.log(typeof(nome))
