3a66c546d78f765c84069a7782d7e05e2fc6f0d7
joaovv17 <joaovibarboa@gmail.com>
2025-07-02 17:45:43 -0300

    Add files via upload

--- testejs.js
+++ testejs.js
@@ -5,3 +5,22 @@ alunos.shift(0);
 console.log(alunos);
 alunos.splice(2, 1);
 console.log(alunos);
+
+
+
+let dormindo = "sono";
+if(dormindo=="sono"){
+    console.log("continuo dormindo");
+}
+else{
+    console.log("tento dormir")
+};
+let comida = {nome:"raviolli" , preço:39 , tipo:"massa"};
+for(let chave in comida){
+    console.log(chave + ":" + comida[chave]);
+}
+let aluno=[
+    {nome:"joao", nota1=4 , nota2=8}];
+for(let media in aluno){
+    console.log(media==(nota1+nota2):2 )
+}
